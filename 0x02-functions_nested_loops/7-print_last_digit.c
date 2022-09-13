47eba98ce055873cae6967942c548f6e93f53fcf
Emmanuel <cocovita2018@gmail.com>
2022-09-13 15:21:17 -0700

    7 print alphabet.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,7 +6,7 @@
  * Return: Value of the last digit.
 */
 
-int print_last_digit(int n):
+int print_last_digit(int n);
 {
 	int last_digit = n % 10;
 
@@ -17,4 +17,3 @@ int print_last_digit(int n):
 
 	return (last_digit);
 }
-
